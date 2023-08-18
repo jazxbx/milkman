11135b8ea032813e0a35b2d5ea91c98606164a77
jazz <jazxbx@gmail.com>
2023-08-18 15:34:35 -0700

    added hire worker functionality

--- script.js
+++ script.js
@@ -1,32 +1,108 @@
 "use strict";
 
+//target the divs
 const cowButton = document.querySelector("#cow");
 const bottleCounter = document.querySelector("#bottle-counter");
 const sellMilk = document.querySelector("#sell-milk");
 const cashCount = document.querySelector("#cash-counter");
+const hireWorker = document.querySelector("#hire-worker");
+const worker = document.querySelector("#workers");
 
 let milkBottles = 0;
 let cash = 0;
-//cow button
-cowButton.addEventListener("click", () => {
-  //shows sell milk text
-  sellMilk.classList.remove("hidden");
-  //increment milk by 1
-  milkBottles += 1;
-  console.log(milkBottles);
-  render();
-});
+
+const milkCorp = [
+  {
+    id: 1,
+    name: "hire a worker",
+    cost: 25,
+    rate: 1,
+    count: 0,
+    icon: "fa-person",
+  },
+  {
+    id: 2,
+    name: "buy an extra cow",
+    cost: 40,
+    rate: 2,
+    count: 0,
+    icon: "fa-cow",
+  },
+  {
+    id: 3,
+    name: "buy another farm",
+    cost: 150,
+    rate: 10,
+    count: 0,
+    icon: "fa-tractor",
+  },
+  {
+    id: 4,
+    name: "become the CEO of Big Milk",
+    cost: 1000,
+    rate: 100,
+    count: 0,
+    icon: "fa-building",
+  },
+  {
+    id: 5,
+    name: "buy Cow Planet",
+    cost: 50000,
+    rate: 1000,
+    count: 0,
+    icon: "fa-earth-oceania",
+  },
+  {
+    id: 6,
+    name: "Rule the multiverse of milk producing planets",
+    cost: 100000,
+    rate: 10000,
+    count: 0,
+    icon: "fa-infinity",
+  },
+];
+
 // render func to update state
 const render = () => {
   bottleCounter.textContent = milkBottles;
   //added to fixed method to add the trailing zeros
   sellMilk.textContent = `Sell Milk  +$${(milkBottles / 2).toFixed(2)}`;
   cashCount.textContent = `+$${cash.toFixed(2)}`;
+  //clears hireEmployee button content. Instead of writing
+  // hireEmployee.innerHTML =''
+  hireWorker.replaceChildren();
 
+  // for of loop checking arr
+  for (let producer of milkCorp) {
+    if (producer.cost <= cash) {
+      const button = document.createElement("button");
+      button.textContent = `${producer.name} -$${producer.cost.toFixed(2)}`;
+      //class affordable makes elements visible
+      button.classList.add("affordable");
+      button.addEventListener("click", () => {
+        cash -= producer.cost;
+        producer.count += 1;
+        render();
+      });
+      hireWorker.appendChild(button);
+    }
+  }
+  //hides sell milk div
   if (milkBottles === 0) {
     sellMilk.classList.add("hidden");
+  } else {
+    sellMilk.classList.remove("hidden");
   }
 };
+
+//cow button
+cowButton.addEventListener("click", () => {
+  //shows sell milk text
+  sellMilk.classList.remove("hidden");
+  //increment milk by 1
+  milkBottles += 1;
+  render();
+});
 
 //sell milk
 sellMilk.addEventListener("click", () => {
@@ -34,3 +110,5 @@ sellMilk.addEventListener("click", () => {
   milkBottles = 0;
   render();
 });
+
+render();
